a31dd3aa4844ecf5bc0f490acb2a7cc31340774a
tabitha muriithi <excellentwriter120@gmail.com>
2024-04-14 15:31:06 +0300

    Attemptedto clear Errors

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from datetime import datetime
 
 from models import db, Message
 
@@ -14,13 +15,63 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.all()
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+    if not messages:
+        return jsonify({'error': 'Message not found'}), 404
+    
+    arr_messages = []
+    for message in messages:
+        message_obj = {
+            'id': message.id,
+            'body': message.body,
+            'username': message.username,               
+            'created_at': message.created_at, 
+            'updated_at': message.updated_at, 
+        }
+        arr_messages.append(message_obj)
+
+    return jsonify(arr_messages), 200
+
+
+@app.route('/createmessages', methods=['POST'])
+def create_message():
+    data = request.json
+    new_message = Message(id=data.get('id'),
+                          body=data.get('body'), 
+                          username=data.get('username'),
+                          created_at=datetime.utcnow(),
+                          updated_at=datetime.utcnow())
+    db.session.add(new_message)
+    db.session.commit()
+
+    response_data = {
+        'message': 'Message created successfully'
+    }
+    return jsonify(response_data)
+
+
+@app.route('/messages/<int:id>', methods=['GET'])
+def get_message_by_id(id):
+    # Get the restaurant by ID
+    message = Message.query.get(id)
+    
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+
+  
+
+    # Create restaurant object with pizzas included
+    message_obj = {
+        'id': message.id,
+        'body': message.body,
+        'username': message.username
+    }
+
+
+    return jsonify(message_obj), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
